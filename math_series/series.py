8c005e0b6db94e7fa649c5ab3ddde41d29c792f7
Tyler Egashira <Wildwoodwaltz@gmail.com>
2022-04-19 21:58:19 -0700

    finished sum_series with passing tests

--- math_series/series.py
+++ math_series/series.py
@@ -21,3 +21,19 @@ def lucas(n):
         return 1
     else:
         return(lucas(n-1)+ lucas(n-2))
+
+def sum_series(n, x=0, y=1):
+    '''
+    Sum Series takes three arguments
+    First argument n is the position in the series you'd like to know.
+    Second argument is x which represents first number in the series.
+    Third argument is y which represents the second number in the series.
+    Then it returns the number at the position n if each number was added to the previous number given two starting numbers
+    If no x or y is givin it will return the fibonacci sequence.
+    '''
+    if n == 1:
+        return x
+    elif n == 2:
+        return y
+    else:
+        return(sum_series(n-1)+ sum_series(n-2))
