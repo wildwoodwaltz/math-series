3b216c18f49b5d1f093f3d99d3ebaa9b37d338c9
Tyler Egashira <Wildwoodwaltz@gmail.com>
2022-04-19 21:29:06 -0700

    added docstring for fibonacci

--- math_series/series.py
+++ math_series/series.py
@@ -1,11 +1,16 @@
 
 def fibonacci(n):
+    '''
+    The Fibonacci sequence is a list of numbers starting with zero where the first value is added to the second value to create a third value. 
+    This occours recursively to infinity. This function will return the fibbonacci number at position given any int input for n 
+    '''
     if n<=1:
         return n
     else:
         return(fibonacci(n-1)+ fibonacci(n-2))
 
 def lucas(n):
+    
     if n == 0:
         return 2
     elif n == 1:
